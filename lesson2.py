9947d592519e1c2ecf4e94df7e6f533aa8b94196
Adithya Solai <ppalanic@terpmail.umd.edu>
2021-01-09 00:38:45 -0500

    updated lesson2 and problem set

--- lesson2.py
+++ lesson2.py
@@ -36,9 +36,23 @@ print(x[-len(x)])
 List Splicing:
 -Get 1 or more values from a list using their position.
 -[inclusive, exclusive]
+-Still follow 0-indexing as we did before
 '''
 print(x[0:2])
 print(x[3:5])
+
+# Can also just leave the second value blank to get the rest of the list:
+print(x[1:])
+
+############ Exercise!! ############
+lst = [8, 0, -6, 79]
+# Retrieve the 3rd element in `lst`
+# A possible answer
+print(lst[2])
+
+# Retrieve the last 3 elements in `lst`
+# A possible answer
+print(lst[1:])
 
 '''
 Append
@@ -61,6 +75,21 @@ print(x)
 print(x)
 x + [-99, -999]
 print(x)
+
+############ Exercise!! ############
+lst = [8, 0, -6, 79] # <- the "original" `lst` in the exercises below
+
+# What is `lst` after the following code?
+lst.append([1,2,3])
+# Answer: [8, 0, -6, 79, [1, 2, 3]]
+
+# What is `lst` after the following code? (using the original `lst`)
+lst + [1, 2, 3]
+# Answer: [8, 0, -6, 79]
+
+# What is `lst` after the following code? (using the original `lst`)
+lst = lst + [1, 2, 3]
+# Answer: [8, 0, -6, 79, 1, 2, 3]
 
 '''
 for-loops + lists
@@ -104,6 +133,33 @@ print(y)
 for i in range(len(y)):
     y[i] = y[i] + 1
 print(y) # everything incremented by 1!
+
+############ Exercise!! ############
+# Replace every integer in `lst` with the sum 
+# from 1 to that integer
+lst = [1, 7, 100, 5]
+
+# Expected answer: [1, 28, 5050, 15]
+
+# Possible Answer:
+for i in range(len(lst)):
+    # Apply Gauss' Sum formula.
+    lst[i] = (lst[i]) * (lst[i] + 1) // 2
+
+print(lst)
+
+# Possible Answer 2:
+lst = [1, 7, 100, 5]
+for i in range(len(lst)):
+    # Use an inner-loop to find the sum of integers
+    # from 1 to the current element of lst
+    sum_of_elem = 0
+    for j in range(1, lst[i]+1):
+        sum_of_elem += j
+
+    lst[i] = sum_of_elem
+    
+print(lst)
 
 ############ enumerate ############
 # need to understand tuples first
--- problem_set2_answers.py
+++ problem_set2_answers.py
@@ -1,5 +1,5 @@
 '''
-Problem Set 2
+Problem Set 2 Answer Key
 Written by Adithya Solai
 
 Copyright © 2021 Adithya Solai. All rights are reserved.
@@ -13,6 +13,9 @@ lst = [7, 43, -2]
 
 # Expected Answer: 7
 
+## Answer ##
+print(lst[0])
+
 #-----------------------------------------------------------------------
 
 ## Problem 7 ##
@@ -20,6 +23,9 @@ lst = [7, 43, -2]
 lst = [7, 43, -2]
 
 # Expected Answer: 43
+
+## Answer ##
+print(lst[1])
 
 #-----------------------------------------------------------------------
 
@@ -29,18 +35,31 @@ lst = [9, 4, 3, -5, 6, 4, 54, 34, 23]
 
 # Expected Answer: [9, 4, 3, -5]
 
+## Answer ##
+print(lst[0:4])
+
 #-----------------------------------------------------------------------
 
 ## Problem 9 ##
 # Show 2 different ways to retrieve the value 5.7 from the list below
 lst = [4, 6, "as", 5.7, -49.2, False]
 
+## Answer ##
+print(lst[3])
+print(lst[-3])
+
 #-----------------------------------------------------------------------
 
 ## Problem 11 ##
 # Given a positive integer n, return a list of all the factors of n.
 def factors(n):
-    return None
+    factor_lst = []
+
+    for i in range(1,n+1):
+        if n % i == 0:
+            factor_lst.append(i)
+
+    return factor_lst
 
 # Test Cases
 assert factors(15) == [1, 3, 5, 15]
@@ -52,7 +71,12 @@ assert factors(23) == [1, 23]
 # Find the sum of all the elements in a given list
 # List is guaranteed to only have positive integers (0, 1, 2, ...)
 def sumList(lst):
-    return None
+    total = 0
+
+    for num in lst:
+        total = total + num
+
+    return total
 
 # Test Cases
 assert sumList([1, 3, 5, 10]) == 19
@@ -68,6 +92,14 @@ assert sumList([99]) == 99
 #######################################################
 # You need to define the function yourself for this one.
 #######################################################
+def largestElem(lst):
+    largest = 0
+
+    for num in lst:
+        if num > largest:
+            largest = num
+    
+    return largest
 
 # Test Cases
 assert largestElem([9, 0, 27, 27]) == 27
@@ -78,10 +110,17 @@ assert largestElem([10]) == 10
 ## Problem 14 ##
 # Find the smallest element in a given list.
 # List is guaranteed to only have positive integers (0, 1, 2, ...)
-# You can assume the list will have at least one element in it.
 #######################################################
 # You need to define the function yourself for this one.
 #######################################################
+def smallestElem(lst):
+    smallest = None
+
+    for num in lst:
+        if smallest is None or num < smallest:
+            smallest = num
+
+    return smallest
 
 # Test Cases
 assert smallestElem([9, 0, 27, 27]) == 0
@@ -90,11 +129,22 @@ assert smallestElem([10]) == 10
 #-----------------------------------------------------------------------
 
 ## Problem 15 ##
-# Condense list of lists into a single list
-# Not every element in the list is a list (see the second test case)
+# Condense a given list of lists into a single list
 #######################################################
 # You need to define the function yourself for this one.
 #######################################################
+def condense(lst):
+    condensed = []
+
+    for elem in lst:
+        if isinstance(elem, list):
+            # Concatenate other list on to `condensed`
+            condensed = condensed + elem
+        else:
+            # Just append the non-list element to `condensed`
+            condensed.append(elem)
+
+    return condensed
 
 # Test Cases
 assert condense([[12,3], ["asdf"], [True, 7]]) == [12, 3, "asdf", True, 7]
