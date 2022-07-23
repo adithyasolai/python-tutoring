6019754a9dd8dd4259358fb3e32c052e8bc7ee74
Adithya Solai <adithyasolai7@gmail.com>
2022-07-23 15:50:13 -0400

    finish lesson2 answer key

--- lessons/answerKeys/lesson2_answers.py
+++ lessons/answerKeys/lesson2_answers.py
@@ -24,3 +24,43 @@ lst + [1, 2, 3]
 # What is `lst` after the following code? (using the original `lst`)
 lst = lst + [1, 2, 3]
 # Answer: [8, 0, -6, 79, 1, 2, 3]
+
+############ Exercise!! ############
+# Replace every integer in `lst` with the sum
+# from 1 to that integer
+lst = [1, 7, 100, 5]
+
+# Expected answer: [1, 28, 5050, 15]
+
+# Possible Answer:
+for i in range(len(lst)):
+    # Apply Gauss' Sum formula.
+    lst[i] = (lst[i]) * (lst[i] + 1) // 2
+
+print(lst)
+
+# Possible Answer 2:
+lst = [1, 7, 100, 5]
+for i in range(len(lst)):
+    # Use an inner-loop to find the sum of integers
+    # from 1 to the current element of lst
+    sum_of_elem = 0
+    for j in range(1, lst[i] + 1):
+        sum_of_elem += j
+
+    lst[i] = sum_of_elem
+
+print(lst)
+
+############ Exercise!! ############
+# Change every value in lst to the string "cat"
+# If the value was already a string, don't change it
+lst = ["asdf", 1, True, 7.7, "cat", ""]
+
+# Expected Answer: ["asdf", "cat", "cat", "cat", "cat", ""]
+
+# A possible answer
+for idx, val in enumerate(lst):
+    if isinstance(val, str) == False:
+        lst[idx] = "cat"
+print(lst)
--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -129,27 +129,9 @@ print(y)  # everything incremented by 1!
 # from 1 to that integer
 lst = [1, 7, 100, 5]
 
+print(lst)
+
 # Expected answer: [1, 28, 5050, 15]
-
-# Possible Answer:
-for i in range(len(lst)):
-    # Apply Gauss' Sum formula.
-    lst[i] = (lst[i]) * (lst[i] + 1) // 2
-
-print(lst)
-
-# Possible Answer 2:
-lst = [1, 7, 100, 5]
-for i in range(len(lst)):
-    # Use an inner-loop to find the sum of integers
-    # from 1 to the current element of lst
-    sum_of_elem = 0
-    for j in range(1, lst[i] + 1):
-        sum_of_elem += j
-
-    lst[i] = sum_of_elem
-
-print(lst)
 
 '''
 Lists are heterogenous
@@ -185,10 +167,6 @@ else:
 # If the value was already a string, don't change it
 lst = ["asdf", 1, True, 7.7, "cat", ""]
 
-# Expected Answer: ["asdf", "cat", "cat", "cat", "cat", ""]
-
-# A possible answer
-for idx, val in enumerate(lst):
-    if isinstance(val, str) == False:
-        lst[idx] = "cat"
 print(lst)
+
+# Expected Answer: ["asdf", "cat", "cat", "cat", "cat", ""]
